f7b0d17cc184caf1ca80de73c9d7f261f61359d1
Kacper Kaperek <kaperekkacper@gmail.com>
2023-11-12 18:21:10 +0100

    Use CloudFront

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,8 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    security_groups = [aws_security_group.lb_sg.id]
+    security_groups = [aws_security_group.lb_sg.id, aws_security_group.cloudfront_sg.id]
+    
   }
 
   egress {
@@ -84,24 +85,148 @@ resource "aws_lb" "web_lb" {
   }
 }
 
+resource "aws_s3_bucket" "web_bucket" {
+  bucket = "kaperekkacper_bucket"
+  tags   = {
+    Name = "kaperekkacper_bucket"
+  }
+}
 
 resource "aws_launch_configuration" "web_lc" {
-  image_id = "ami-0c94855ba95c71c99" 
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.web_sg.id]
+  image_id           = "ami-0c94855ba95c71c99"
+  instance_type      = "t2.micro"
+  security_groups    = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y httpd
-              aws s3 cp s3://kk_web-bucket/index.html /var/www/html/
+              aws s3 cp s3://${aws_s3_bucket.web_bucket.bucket}/index.html /var/www/html/
               systemctl start httpd
+
+              echo "export CDN_URL=https://${aws_cloudfront_distribution.web_cdn.domain_name}" >> /etc/environment
               EOF
 }
 
 resource "aws_autoscaling_group" "web_asg" {
   min_size             = 1
-  desired_capacity     = 2
-  max_size             = 3
+  desired_capacity     = 1
+  max_size             = 2
   launch_configuration = aws_launch_configuration.web_lc.name
+}
+
+resource "aws_security_group" "cloudfront_sg" {
+  name_prefix = "cloudfront_sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.web_bucket.website_domain
+    origin_id                = aws_s3_bucket.web_bucket.bucket_regional_domain_name
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "mylogs.s3.amazonaws.com"
+    prefix          = "myprefix"
+  }
+
+  aliases = ["mysite.example.com", "yoursite.example.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.web_bucket.bucket_regional_domain_name
+
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.web_bucket.bucket_regional_domain_name
+
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
